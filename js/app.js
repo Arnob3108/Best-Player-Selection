9ca9f9bedd0614a00b43407e96b6943dd8ed1339
Arnob3108 <sajedurrahmanarnob@gmail.com>
2022-08-23 13:14:54 +0600

    design and selected players mostly done

---
+++ js/app.js
@@ -0,0 +1,34 @@
+let selectedPlayers = [];
+let count = 0;
+let buttons = document.querySelectorAll('.btn-primary');
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        let playerName = button.parentNode.children[0].innerText;
+        let player = {
+            playerName: playerName,
+        };
+
+        selectedPlayers.push(player);
+
+        let selectedPlayersContainer = document.getElementById('selecting-players');
+        selectedPlayersContainer.textContent = '';
+        for (let i = 0; i < selectedPlayers.length; i++) {
+            let li = document.createElement('li');
+            li.classList = 'list';
+            li.innerHTML = selectedPlayers[i].playerName;
+            selectedPlayersContainer.appendChild(li);
+            count = i + 1;
+
+            if (selectedPlayersContainer.childNodes.length > 5) {
+                removeLastChild('selecting-players');
+                button.removeAttribute('disabled');
+                alert("You can't add More than 5  players");
+
+                return;
+            }
+            setTextElementValueById('count', count);
+        }
+        button.setAttribute('disabled', true);
+        button.style.backgroundColor = '#758283';
+    });
+}
---
+++ js/utilities.js
@@ -0,0 +1,21 @@
+function getInputFieldValueById(inputId) {
+    let inputField = document.getElementById(inputId);
+    let inputFieldValue = parseFloat(inputField.value);
+    return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+    let element = document.getElementById(elementId);
+    let elementValue = parseFloat(element.innerText);
+    return elementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+    let element = document.getElementById(elementId);
+    element.innerText = newValue;
+}
+
+function removeLastChild(elementId) {
+    let element = document.getElementById(elementId);
+    element.removeChild(element.lastChild);
+}
